20472302c5918d4079a0799b1fc0e18f7334edb1
JT Raber <JMANN240raber@gmail.com>
2026-04-09 10:01:51 -0400

    utility methods

--- lib/src/database/mod.rs
+++ lib/src/database/mod.rs
@@ -19,6 +19,10 @@ impl DbRace {
     pub async fn insert_participant(&self, pool: &SqlitePool, name: String, time: TimeDelta) -> sqlx::Result<DbRaceParticipant> {
         DbRaceParticipant::insert(pool, self.id, name, time).await
     }
+
+    pub async fn get_participants(&self, pool: &SqlitePool) -> sqlx::Result<Vec<DbRaceParticipant>> {
+        DbRaceParticipant::get_by_race_id(pool, self.id).await
+    }
 }
 
 pub struct DbRaceParticipant {
@@ -28,13 +32,23 @@ pub struct DbRaceParticipant {
 }
 
 impl DbRaceParticipant {
-    pub async fn insert(pool: &SqlitePool, id: i64, name: String, time: TimeDelta) -> sqlx::Result<Self> {
+    pub async fn get_by_race_id(pool: &SqlitePool, race_id: i64) -> sqlx::Result<Vec<Self>> {
+        query_as!(
+            Self,
+            "SELECT * FROM race_participant WHERE id = ? ORDER BY time ASC",
+            race_id,
+        )
+        .fetch_all(pool)
+        .await
+    }
+
+    pub async fn insert(pool: &SqlitePool, race_id: i64, name: String, time: TimeDelta) -> sqlx::Result<Self> {
         let seconds = time.as_seconds_f64();
 
         query_as!(
             Self,
             "INSERT INTO race_participant VALUES (?, ?, ?) RETURNING *",
-            id,
+            race_id,
             name,
             seconds,
         )
